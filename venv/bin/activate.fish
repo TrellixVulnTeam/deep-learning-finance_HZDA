850c1517419fe5ed8d4438aa5c32c3bf9df1bc76
john collins <john.collins@edhec.com>
2022-04-28 20:21:46 +0800

    v0.1.1, updated notebook

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/climatemetrics/posts/dl-finance/venv"
+set -gx VIRTUAL_ENV "/home/climatemetrics/posts/deep-learning-finance/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
